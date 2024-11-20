f6f1e6d22ec64ea81f225c880cfd183c65b44b93
Oritseweyinmi Samuel Tuoyo <samueltuoyo9082@gmail.com>
2024-11-20 12:47:50 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
-import MainLayout from './pages/MainLayout'
-import Blog from './pages/Blog'
-function App() {
-   const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path="/" element={<MainLayout />} />
-    <Route path="/blog" element={<Blog />} />
-    </>
-    )
-)
-  
-  return <RouterProvider router={router} />
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './pages/MainLayout';
+import Blog from './pages/Blog';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
